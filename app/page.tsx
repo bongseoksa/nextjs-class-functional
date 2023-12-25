b0b66b4791ad79714b5b915316e4e2d5713a0268
Todd <bssa@maxst.com>
2023-12-26 08:42:23 +0900

    [Commit 내용] state * 함수형 컴포넌트 state 활용법 추가

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,32 @@ export default function Home() {
 export const FunctionComp = (props: { initNumber: number }) => {
   const [number, setNumber] = useState<number>(props.initNumber);
 
+  // useSate 활용1.
+  // const [date, setDate] = useState<string>(new Date().toString());
+
+  // useState 활용2. 활용1과 동일한 기능
+  const dateState = useState<string>(new Date().toString());
+  let date = dateState[0];
+  const setDate = dateState[1];
+
   return (
     <div className={styles.container}>
       <h2>function style Component</h2>
       <p>Number : {number}</p>
+      <p>Date : {date}</p>
       <input
         type="button"
         value="random"
         onClick={() => {
           setNumber(Math.random());
         }}
+      />
+      <input
+        type="button"
+        value="date"
+        onClick={(() => {
+          setDate(new Date().toString());
+        }).bind(this)}
       />
     </div>
   );
@@ -34,6 +50,7 @@ export const FunctionComp = (props: { initNumber: number }) => {
 export class ClassComp extends React.Component {
   state: Readonly<{}> = {
     number: this.props.initNumber,
+    date: new Date().toString(),
   };
 
   render() {
@@ -42,11 +59,19 @@ export class ClassComp extends React.Component {
       <div className={styles.container}>
         <h2>class style Component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input
           type="button"
           value="random"
           onClick={(() => {
             this.setState({ number: Math.random() });
+          }).bind(this)}
+        />
+        <input
+          type="button"
+          value="date"
+          onClick={(() => {
+            this.setState({ date: new Date().toString() });
           }).bind(this)}
         />
       </div>
